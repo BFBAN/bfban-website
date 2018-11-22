02e7399d45b0ebf3ebfe49376014e482c359c2cd
Goare <mygoare@gmail.com>
2018-11-22 17:51:53 +0800

    iview 多语言支持

--- front/src/index.js
+++ front/src/index.js
@@ -24,140 +24,15 @@ Vue.use(VueQuillEditor);
 
 FastClick.attach(document.body);
 
+import { cn, uk } from './lang/lang';
+
+
+// iview.lang('en-US');
+
 const locale = 'cn';
 const messages = {
-  cn: {
-    header: {
-      index: '首页',
-      about: '关于',
-      cheaters: '外挂公示',
-      report: '举报作弊',
-      signin: '登录',
-      signup: '注册',
-      signout: '注销',
-      daily: 'ban日报',
-      dashboard: '管理后台',
-    },
-    footer: {
-      author: '由 mygoare 开发',
-    },
-    home: {
-      activity: {
-        title: '网站动态',
-      },
-      howToUse: {
-        title: '如何使用本站',
-        tools: '小工具',
-        qqGroup: 'QQ群',
-      },
-    },
-    about: {
-      title: '关于',
-    },
-    signin: {
-      title: '登录',
-      form: {
-        username: '用户名',
-        password: '密码',
-        captcha: '验证码',
-        getCaptcha: '获得验证码',
-        submit: '提交',
-        submitHint: '没有账号？去注册',
-      },
-    },
-    signup: {
-      title: '注册',
-      form: {
-        username: '用户名',
-        password: '密码',
-        originId: '橘子ID',
-        qq: 'QQ',
-        captcha: '验证码',
-        getCaptcha: '获得验证码',
-        submit: '提交',
-        submitHint: '已有账号？去登录',
-      },
-    },
-    list: {
-      title: '外挂公示',
-      filters: {
-        game: {
-          bf1: '战地1',
-          bfv: '战地v',
-        },
-        status: {
-          all: '全部',
-        },
-        refresh: '刷新',
-      },
-    },
-  },
-  uk: {
-    header: {
-      index: 'Home',
-      about: 'About',
-      cheaters: 'Cheaters',
-      report: 'Report',
-      signin: 'SignIn',
-      signup: 'SignUp',
-      signout: 'SignOut',
-      daily: 'BanDaily',
-      dashboard: 'Dashboard',
-    },
-    footer: {
-      author: 'Written by mygoare',
-    },
-    home: {
-      activity: {
-        title: 'Activity',
-      },
-      howToUse: {
-        title: 'How to use',
-        tools: 'Tools',
-        qqGroup: 'QQ Group',
-      },
-    },
-    about: {
-      title: 'About',
-    },
-    signin: {
-      title: 'Sign In',
-      form: {
-        username: 'Username',
-        password: 'Password',
-        captcha: 'Captcha',
-        getCaptcha: 'getCaptcha',
-        submit: 'Submit',
-        submitHint: 'Do not have account? Go Sign up',
-      },
-    },
-    signup: {
-      title: 'Sign Up',
-      form: {
-        username: 'Username',
-        password: 'Password',
-        originId: 'originId',
-        qq: 'QQ',
-        captcha: 'Captcha',
-        getCaptcha: 'Get Captcha',
-        submit: 'Submit',
-        submitHint: 'Hava account? Go Sign In',
-      },
-    },
-    list: {
-      title: 'Cheaters Show',
-      filters: {
-        game: {
-          bf1: 'bf1',
-          bfv: 'bfv',
-        },
-        status: {
-          all: 'all',
-        },
-        refresh: 'refresh',
-      },
-    },
-  },
+  cn: Object.assign(cn, iview.langs['zh-CN']),
+  uk: Object.assign(uk, iview.langs['en-US']),
 };
 
 const i18n = new VueI18n({
@@ -168,7 +43,7 @@ const i18n = new VueI18n({
 const root = document.createElement('div');
 document.body.appendChild(root);
 
-const app =new Vue({
+const app = new Vue({
   router,
   store,
   i18n,
---
+++ front/src/lang/lang.js
@@ -0,0 +1,137 @@
+const cn = {
+  header: {
+    index: '首页',
+    about: '关于',
+    cheaters: '外挂公示',
+    report: '举报作弊',
+    signin: '登录',
+    signup: '注册',
+    signout: '注销',
+    daily: 'ban日报',
+    dashboard: '管理后台',
+  },
+  footer: {
+    author: '由 mygoare 开发',
+  },
+  home: {
+    activity: {
+      title: '网站动态',
+    },
+    howToUse: {
+      title: '如何使用本站',
+      tools: '小工具',
+      qqGroup: 'QQ群',
+    },
+  },
+  about: {
+    title: '关于',
+  },
+  signin: {
+    title: '登录',
+    form: {
+      username: '用户名',
+      password: '密码',
+      captcha: '验证码',
+      getCaptcha: '获得验证码',
+      submit: '提交',
+      submitHint: '没有账号？去注册',
+    },
+  },
+  signup: {
+    title: '注册',
+    form: {
+      username: '用户名',
+      password: '密码',
+      originId: '橘子ID',
+      qq: 'QQ',
+      captcha: '验证码',
+      getCaptcha: '获得验证码',
+      submit: '提交',
+      submitHint: '已有账号？去登录',
+    },
+  },
+  list: {
+    title: '外挂公示',
+    filters: {
+      game: {
+        bf1: '战地1',
+        bfv: '战地v',
+      },
+      status: {
+        all: '全部',
+      },
+      refresh: '刷新',
+    },
+  },
+};
+const uk = {
+  header: {
+    index: 'Home',
+    about: 'About',
+    cheaters: 'Cheaters',
+    report: 'Report',
+    signin: 'SignIn',
+    signup: 'SignUp',
+    signout: 'SignOut',
+    daily: 'BanDaily',
+    dashboard: 'Dashboard',
+  },
+  footer: {
+    author: 'Written by mygoare',
+  },
+  home: {
+    activity: {
+      title: 'Activity',
+    },
+    howToUse: {
+      title: 'How to use',
+      tools: 'Tools',
+      qqGroup: 'QQ Group',
+    },
+  },
+  about: {
+    title: 'About',
+  },
+  signin: {
+    title: 'Sign In',
+    form: {
+      username: 'Username',
+      password: 'Password',
+      captcha: 'Captcha',
+      getCaptcha: 'getCaptcha',
+      submit: 'Submit',
+      submitHint: 'Do not have account? Go Sign up',
+    },
+  },
+  signup: {
+    title: 'Sign Up',
+    form: {
+      username: 'Username',
+      password: 'Password',
+      originId: 'originId',
+      qq: 'QQ',
+      captcha: 'Captcha',
+      getCaptcha: 'Get Captcha',
+      submit: 'Submit',
+      submitHint: 'Hava account? Go Sign In',
+    },
+  },
+  list: {
+    title: 'Cheaters Show',
+    filters: {
+      game: {
+        bf1: 'bf1',
+        bfv: 'bfv',
+      },
+      status: {
+        all: 'all',
+      },
+      refresh: 'refresh',
+    },
+  },
+};
+
+export {
+  cn,
+  uk,
+};
