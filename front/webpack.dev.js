af2c398052bcc6d85151df65e6e86a7bb4a7fb00
Goare <mygoare@gmail.com>
2018-12-17 23:55:32 +0800

    webpack-dev-server (yarn serve & watch)

--- front/webpack.dev.js
+++ front/webpack.dev.js
@@ -4,7 +4,14 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
-  // devServer: {
-  //   contentBase: './dist',
-  // },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true,
+    contentBase: '../server/public',
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+    poll: 1000,
+  },
 });
