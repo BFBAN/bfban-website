4038cacb6be896a68e8253eec63f4c666341cc25
Goare <mygoare@gmail.com>
2018-12-17 23:49:23 +0800

    app server cors settings

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const csrf = require('csurf');
 const moment = require('moment');
+const { check, validationResult } = require('express-validator/check');
 
 // const { checkGameIdExist } = require('../libs/webshot');
+const { verifyJWTMiddleware, verifyAdminPrivilegeMiddleware } = require('../middlewares/auth');
 const { addOneDay, convertDatetimeToTimeZone } = require('../libs/misc');
 const { getUserInfo } = require('../libs/origin');
 
 const router = express.Router();
+
+// cors options
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.header('Origin')); // better than wildcard *
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 // csrf protection
 const csrfProtection = csrf({ cookie: true });
@@ -17,20 +28,33 @@ const grabTeamliquid = require('../scraper/teamliquid');
 const { getCaptcha } = require('../libs/captcha');
 const db = require('../mysql');
 
-router.get('/', csrfProtection, (req, res, next) => {
-  res.render('index', { title: '战地风云联BAN调查局', csrfToken: req.csrfToken() });
-});
+async function getReportNum() {
+  const tmp = await db.query('select count(*) as num from user_report_cheater where valid = "1"');
+  return tmp[0].num;
+}
 
-router.get('/youku/:name', async (req, res) => {
-  res.json(await grabYouku(req.params.name));
-});
+async function getUserNum() {
+  const tmp = await db.query('select count(*) as num from users where valid = "1"');
+  return tmp[0].num;
+}
 
-router.get('/teamliquid/*', async (req, res) => {
-  console.log(req.params);
-  res.json(await grabTeamliquid(req.params[0]));
-});
+async function getCheaterNum() {
+  const tmp = await db.query('select count(*) as num from cheaters where status = "1" and valid = "1"');
+  return tmp[0].num;
+}
 
-router.post('/checkGameIdExist', async (req, res) => {
+// router.get('/', csrfProtection, (req, res, next) => {
+//   res.render('index', { title: '战地风云联BAN调查局', csrfToken: req.csrfToken() });
+// });
+
+router.post('/checkGameIdExist', [
+  check('id').not().isEmpty(),
+], verifyJWTMiddleware, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(200).json({ error: 1, msg: '请规范填写', errors: errors.array() });
+  }
+
   const { id } = req.body;
   let idExist;
 
@@ -48,9 +72,6 @@ router.post('/checkGameIdExist', async (req, res) => {
   });
 });
 
-
-// home page routes
-
 // ?id=fxodo
 router.get('/search', async (req, res, next) => {
   const { id } = req.query;
@@ -58,7 +79,7 @@ router.get('/search', async (req, res, next) => {
   // if false, invalid originId
   // const idExist = await checkGameIdExist(id);
 
-  const cheatersQueryResult = await db.query(`select DISTINCT(cheaterGameName) as originId, originUserId from origin where cheaterGameName like ?`, [`%${id}%`]);
+  const cheatersQueryResult = await db.query('select DISTINCT(cheaterGameName) as originId, originUserId from origin where cheaterGameName like ?', [`%${id}%`]);
   // const cheatersQueryResult = await db.query('select originId, status, originUserId, createDatetime, game from cheaters where valid = \'1\' and originId like ?', [`%${id}%`]);
 
   return res.json({
@@ -68,21 +89,6 @@ router.get('/search', async (req, res, next) => {
     },
   });
 });
-
-async function getReportNum() {
-  const tmp = await db.query('select count(*) as num from user_report_cheater where valid = "1"');
-  return tmp[0].num;
-}
-
-async function getUserNum() {
-  const tmp = await db.query('select count(*) as num from users where valid = "1"');
-  return tmp[0].num;
-}
-
-async function getCheaterNum() {
-  const tmp = await db.query('select count(*) as num from cheaters where status = "1" and valid = "1"');
-  return tmp[0].num;
-}
 
 // activities (reports, verifies, registers)
 router.get('/activity', async (req, res) => {
@@ -126,11 +132,25 @@ router.use('/captcha', getCaptcha);
 
 router.use('/auth', require('./auth'));
 
-// router.use('/upload', require('./upload'));
-
 // signin, signup, report, verify
 router.use('/account', require('./user'));
 router.use('/cheaters', require('./cheater'));
+
+
+// test purpose
+
+// router.get('/youku/:name', async (req, res) => {
+//   res.json(await grabYouku(req.params.name));
+// });
+
+// router.get('/teamliquid/*', async (req, res) => {
+//   console.log(req.params);
+//   res.json(await grabTeamliquid(req.params[0]));
+// });
+
+// router.use('/upload', require('./upload'));
+
+// test purpose
 
 
 module.exports = router;
