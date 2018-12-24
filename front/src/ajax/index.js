582e1c49469c66ef8ce4ca9c4fbe68b6ae5f56c3
Goare <mygoare@gmail.com>
2018-12-24 23:19:37 +0800

    fix

--- front/src/ajax/index.js
+++ front/src/ajax/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const baseURL = 'https://test.com/api';
-const baseURL = 'http://localhost:4000/api';
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://bfban.com/api' : 'http://localhost:4000/api';
 
 const ajax = axios.create({
   baseURL,
