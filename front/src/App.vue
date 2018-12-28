1a856eb98fc449a9cae0a2d0bb6f5bb54134e707
Goare <mygoare@gmail.com>
2018-12-28 17:43:01 +0800

    update 2019 theme

--- front/src/App.vue
+++ front/src/App.vue
@@ -46,6 +46,19 @@ export default {
 
     /*merry 2018 christmas*/
     background-image: url('https://i.loli.net/2018/12/24/5c20cecbde3c9.jpg');
+
+
+    /*2019 yellow right bottom*/
+    background-image: url(https://i.loli.net/2018/12/28/5c25eeed25496.png);
+    background-position: right bottom;
+    background-size: 24rem;
+    background-color: #fdd94194;
+
+    /*2019 center bottom pig pink*/
+    background-image: url(https://i.loli.net/2018/12/28/5c25ed58ef2f2.png);
+    background-position: center bottom;
+    background-size: 19rem;
+    background-color: #ffd5d673;
   }
   .app {
     height: 100%;
