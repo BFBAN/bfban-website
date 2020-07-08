ec5472bcb85c56098130a2d4479aad113b5fed58
Goare <mygoare@gmail.com>
2020-07-08 17:48:41 +0800

    test api v1

--- front/src/mixins/ajax.js
+++ front/src/mixins/ajax.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://api.bfban.com';
+const baseURL = 'https://api.bfban.com/v1';
 // const baseURL = 'http://127.0.0.1:4000/api';
 // const baseURL = process.env.NODE_ENV === 'production' ? 'https://bfban.com/api' : 'http://127.0.0.1:4000/api';
 
