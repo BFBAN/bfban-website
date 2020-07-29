97e6c1c4e1ed6c1f38c5583b52b70323f8b8279f
Cabbagelol <nickmiku@foxmail.com>
2020-07-29 15:56:10 +0800

    Update Bulletin.vue
    BFBAN APP URL

--- front/src/components/Bulletin.vue
+++ front/src/components/Bulletin.vue
@@ -13,7 +13,7 @@
       return {
         data: marked(`
 **网站开发**: [mygoare](https://mygoare.com)
-**BFBAN APP下载**: [cabbagelol](https://app.bfban.com/)
+**BFBAN APP下载**: [cabbagelol](https://app.bfban.com/public/www/)
 **网站服务器提供**: [AWSL服务器](https://battlefield.now.sh/)
         `, {breaks: true, gfm: true, smartLists: true})
       }
