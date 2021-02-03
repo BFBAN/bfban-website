a74dae50a78562ae870b552d96cb839304a2ea37
Goare <mygoare@gmail.com>
2021-02-03 21:19:36 +0800

    fix npm taobao fail error bug... (in yarn.lock)

--- server/routes/cheater.js
+++ server/routes/cheater.js
@@ -276,7 +276,7 @@ router.post('/', verifyJWTMiddleware, [
   check('originId').not().isEmpty().isAscii(),
   check('cheatMethods').not().isEmpty(),
   check('bilibiliLink').optional({ checkFalsy: true }).isURL({allow_protocol_relative_urls: true}),
-  check('captcha').not().isEmpty().isLength({ min: 4, max: 4 }),
+  // check('captcha').not().isEmpty().isLength({ min: 4, max: 4 }),
   check('description').not().isEmpty(),
 
   check('originUserId').not().isEmpty(),
