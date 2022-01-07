e6f6d9ba81d61efe97a9b2397b85a83ad071852c
glass-panel <63582311+glass-panel@users.noreply.github.com>
2022-01-07 16:36:35 +0800

    config example

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": false,
+        "es2021": true,
+		"node": true,
+    },
+    "extends": "eslint:recommended",
+	"parser": "@babel/eslint-parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module",
+        "requireConfigFile": false
+    },
+	"ignorePatterns": ["typedef.js"],
+    "rules": {
+		"no-unused-vars": 0,
+		"no-constant-condition": 0,
+    }
+};
