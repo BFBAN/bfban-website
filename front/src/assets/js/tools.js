9b60ebc5c7946b4dae4ad6b5cc26e2701d233a0f
Cabbagelol <nickmiku@foxmail.com>
2022-08-31 23:12:42 +0800

    pull (#21)
    * 关闭private文件夹的git忽略，新增privateApi.js文件
    
    * 同上
    
    * commit
    
    * 修改http.js 初始化axios 错误
    
    * 上传图片
    
    * Create Dockerfile
    
    * Create main.yml
    
    * commit
    
    * del
    
    del -file
    
    Co-authored-by: lin_RunRun <841850130@qq.com>
    Co-authored-by: iiTzArcur <zefanjaj@gmail.com>

---
+++ front/src/assets/js/tools.js
@@ -0,0 +1,43 @@
+import { http, api, http_token} from "./"
+import store from '@/store'
+export function upload(file) {
+  const size = 2*1024*1024
+  return new Promise(function (resolve, reject) {
+    if(file.size <= size) {
+      fetch(http.getUrl() + api["service_upload"], {
+        method: 'PUT',
+        headers: {
+          ["Content-Type"]: file.type,
+          ["Content-Length"]: file.size,
+          ['x-access-token']: store.state.user.token
+        },
+        body: file.slice(0, file.length)
+      }).then(res => res.json()).then(res => {
+        service_file(res)
+      })
+    }else {
+      fetch(http.getUrl() + api["service_uploadBigFile"], {
+        method: 'POST',
+        data: { 
+          "size": file.type,
+          "mimeType": file.size
+         },
+         headers: {
+          ['x-access-token']: store.state.user.token
+         },
+        body: file.slice(0, file.length)
+      }).then(res => res.json()).then(res => {
+        service_file(res)
+      })
+    }
+    function service_file(res) {
+      http.get(api["service_file"], {
+        params: {
+          filename: res.data.name, explain: true
+        }
+      }).then(res => {
+        resolve(res.data.data.downloadURL)
+      })
+    }
+  })
+}
