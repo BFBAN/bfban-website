cb40edbece606fd387d0bc84c4eaa1302b6c6638
lin_RunRun <841850130@qq.com>
2022-09-17 10:32:44 +0800

    commit

--- front/src/main.js
+++ front/src/main.js
@@ -41,6 +41,7 @@ Vue.config.productionTip = false
 
 let cookieUser = Cookies.get('user') && JSON.parse(Cookies.get('user'));
 cookieUser = cookieUser || {}
+console.log('cookieUser---------', cookieUser)
 if(!cookieUser.token) {
   Cookies.remove('user');
 }
