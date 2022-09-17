ced4a7cdff4929c9ce242eadd8aa6803a844098f
lin_RunRun <841850130@qq.com>
2022-09-17 11:05:06 +0800

    commit

--- front/vue.config.js
+++ front/vue.config.js
@@ -16,13 +16,13 @@ module.exports = {
 
   // PWA
   // https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa
-  pwa: {
-    name: 'BFBAN',
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      swSrc: 'src/service-worker.js'
-    }
-  },
+  // pwa: {
+  //   name: 'BFBAN',
+  //   workboxPluginMode: 'InjectManifest',
+  //   workboxOptions: {
+  //     swSrc: 'src/service-worker.js'
+  //   }
+  // },
 
   pluginOptions: {
     i18n: {
@@ -43,37 +43,37 @@ module.exports = {
     }
   },
 
-  // 定义资源方式
-  configureWebpack: {
-    output: {
-      filename: `assets/js/[name].${packageConf.version}.js`,
-      chunkFilename: `assets/js/[name].${packageConf.version}.js`
-    },
-    plugins: [
-      new SitemapPlugin({
-        // Production Url
-        base: url.protocol + '://' + url.host,
-        paths: ['/', '/player', '/sitestats', '/search'],
-        options:{
-          filename: 'sitemap.xml',
-          lastmod: true,
-          changefreq: 'hourly',
-          priority: .7
-        }
-      }),
-      new InjectManifest({
-        swSrc: "src/service-worker.js",
-      }),
-      new CopyWebpackPlugin(
-          [{
-            from: `src/lang`,
-            to: `lang`
-          }],
-          {
-            // 无论是否修改，全部复制
-            copyUnmodified: true
-          }
-      ),
-    ],
-  },
+  // // 定义资源方式
+  // configureWebpack: {
+  //   output: {
+  //     filename: `assets/js/[name].${packageConf.version}.js`,
+  //     chunkFilename: `assets/js/[name].${packageConf.version}.js`
+  //   },
+  //   plugins: [
+  //     new SitemapPlugin({
+  //       // Production Url
+  //       base: url.protocol + '://' + url.host,
+  //       paths: ['/', '/player', '/sitestats', '/search'],
+  //       options:{
+  //         filename: 'sitemap.xml',
+  //         lastmod: true,
+  //         changefreq: 'hourly',
+  //         priority: .7
+  //       }
+  //     }),
+  //     new InjectManifest({
+  //       swSrc: "src/service-worker.js",
+  //     }),
+  //     new CopyWebpackPlugin(
+  //         [{
+  //           from: `src/lang`,
+  //           to: `lang`
+  //         }],
+  //         {
+  //           // 无论是否修改，全部复制
+  //           copyUnmodified: true
+  //         }
+  //     ),
+  //   ],
+  // },
 }
