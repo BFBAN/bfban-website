8316978bd5eeacc1192f515d0e4675f83f6407e8
cabbagelol <nickmiku@foxmail.com>
2022-09-22 11:00:01 +0000

    build front

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.73136458ac92cb37a4ed06c43cb915aa.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.36ff761e37a9353c16f97004b87487fd.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.addEventListener('fetch', function (event) {
   console.log(event);
