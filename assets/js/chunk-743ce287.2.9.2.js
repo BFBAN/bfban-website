80e8449524046b2b7539608233e49de9e7359853
cabbagelol <nickmiku@foxmail.com>
2022-11-21 12:47:19 +0000

    build front

--- assets/js/chunk-743ce287.2.9.2.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-743ce287"],{"0354":function(n,w,c){}}]);
-//# sourceMappingURL=chunk-743ce287.2.9.2.js.map
--- registerServiceWorker.js
+++ registerServiceWorker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.68df59fbee110421be6b829ed62ab32a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.73cc06e2480575e95113c30747949174.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-console */
 import { register } from 'register-service-worker'
