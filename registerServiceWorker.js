92970cacd483837c3eb7d836e722588152b9d16b
cabbagelol <nickmiku@foxmail.com>
2023-01-30 16:07:09 +0000

    build front

--- registerServiceWorker.js
+++ registerServiceWorker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.d9931128befecfcb155800fd7308412c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.54448b2b0bdb59b32f9f3f847d73f8e5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-console */
 import { register } from 'register-service-worker'
