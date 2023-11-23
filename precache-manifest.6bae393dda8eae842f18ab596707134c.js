006a4a1aba3ec5e3005e8603118bf16eba5c9757
cabbagelol <nickmiku@foxmail.com>
2023-11-23 19:53:43 +0000

    build front

--- precache-manifest.6bae393dda8eae842f18ab596707134c.js
+++ precache-manifest.6bae393dda8eae842f18ab596707134c.js
@@ -12,11 +12,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/_redirects"
   },
   {
-    "revision": "327970c9d89f43a66734",
+    "revision": "2e1351dc8af8169c8197",
     "url": "/assets/css/app.bd1c0928.css"
   },
   {
-    "revision": "327970c9d89f43a66734",
+    "revision": "2e1351dc8af8169c8197",
     "url": "/assets/css/app.bd1c0928.css.map"
   },
   {
@@ -312,11 +312,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/assets/fonts/ionicons.d535a25a.ttf"
   },
   {
-    "revision": "327970c9d89f43a66734",
+    "revision": "2e1351dc8af8169c8197",
     "url": "/assets/js/app.2.9.11.js"
   },
   {
-    "revision": "327970c9d89f43a66734",
+    "revision": "2e1351dc8af8169c8197",
     "url": "/assets/js/app.2.9.11.js.map"
   },
   {
